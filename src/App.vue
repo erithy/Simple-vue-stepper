f47dcdd23cf8aa49f6dff32ad276cffddf3f8c89
Rithy <eang.rithy@mbanq.com>
2024-06-21 17:31:55 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,26 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    sdfsaf
+    <v-stepper :steps="steps" v-model="step"></v-stepper>
+{{step}}
+    <template v-if="step === 1">asdfasdfsadf<!-- Step 1 Content --></template>
+    <template v-if="step === 2">asdfsdaf<!-- Step 2 Content --></template>
+    <template v-if="step === 3">3333<!-- Step 3 Content --></template>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VStepper from './components/Stepper.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    VStepper
+  },
+  data: () => ({ steps: 3, step: undefined })
 }
 </script>
 
